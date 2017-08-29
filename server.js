9035375a23964b560a50cad64316d8b737d440e2
vlotysh <Gangsta1@i.ua>
2017-08-29 22:53:46 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
+var playersList = {};
+
+app.set('port', 5000);
+app.use('/static', express.static(__dirname + '/static'));
+ 
+// Маршруты
+app.get('/', function(request, response) {
+    response.sendFile(path.join(__dirname, 'index.html'));
+});
+ 
+// Запуск сервера
+server.listen(5000, function() {
+    console.log('Запускаю сервер на порте 5000');
+});
+
+
+var players = {};
+io.on('connection', function(socket) {
+    const _id = socket.id
+
+    socket.on('disconnect', function() {
+        console.log('playerDiconected');
+        var name = playersList[_id];
+        delete players[_id];
+        delete playersList[_id];
+        io.sockets.emit('playerDiconected', name);
+    });
+
+    socket.on('new player', function(name) {
+
+        playersList[socket.id] = name;
+        players[socket.id] = {
+            x: 300,
+            y: 300
+        };
+
+        io.sockets.emit('playerJoined', name);
+    });
+
+    socket.on('chat', function(text) {
+
+        message = {};
+        message['player'] = playersList[socket.id];
+        message['text'] = text;
+        console.log(message);
+        io.sockets.emit('chatList', message);
+    });
+
+    socket.on('movement', function(data) {
+        var player = players[socket.id] || {};
+        if (data.left) {
+            player.x -= 5;
+        }
+        if (data.up) {
+            player.y -= 5;
+        }
+        if (data.right) {
+            player.x += 5;
+        }
+        if (data.down) {
+            player.y += 5;
+        }
+    });
+});
+
+setInterval(function() {
+    io.sockets.emit('state', players);
+}, 1000 / 60);
---
+++ static/game.js
@@ -0,0 +1,107 @@
+name = prompt("Enter your name");
+
+
+
+
+var socket = io();
+socket.on('message', function(data) {
+    console.log(data);
+});
+
+var movement = {
+    up: false,
+    down: false,
+    left: false,
+    right: false
+}
+document.addEventListener('keydown', function(event) {
+
+    if($('#chatBar input').is(":focus")) {
+        return false;
+    };
+
+    switch (event.keyCode) {
+        case 65: // A
+            movement.left = true;
+            break;
+        case 87: // W
+            movement.up = true;
+            break;
+        case 68: // D
+            movement.right = true;
+            break;
+        case 83: // S
+            movement.down = true;
+            break;
+    }
+});
+document.addEventListener('keyup', function(event) {
+    if($('#chatBar input').is(":focus")) {
+        return false;
+    };
+
+    switch (event.keyCode) {
+        case 65: // A
+            movement.left = false;
+            break;
+        case 87: // W
+            movement.up = false;
+            break;
+        case 68: // D
+            movement.right = false;
+            break;
+        case 83: // S
+            movement.down = false;
+            break;
+    }
+});
+
+socket.emit('new player', name);
+setInterval(function() {
+    if($('#chatBar input').is(":focus")) {
+        return false;
+    };
+    socket.emit('movement', movement);
+}, 1000 / 60);
+
+var canvas = document.getElementById('canvas');
+canvas.width = 800;
+canvas.height = 600;
+var context = canvas.getContext('2d');
+
+socket.on('state', function(players) {
+    context.clearRect(0, 0, 800, 600);
+    context.fillStyle = 'green';
+    for (var id in players) {
+        var player = players[id];
+        context.beginPath();
+        context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
+        context.fill();
+    }
+});
+
+socket.on('chatList', function(message) {
+
+    $('#chatList ul').append('<li> <strong>'+message['player']+'</strong>' +message['text']+ '</li>');
+
+    console.log(message);
+})
+
+
+socket.on('playerDiconected', function(player) {
+
+    $('#chatList ul').append("<li> Player <strong>"+player+"</strong> diconected!</li>");
+})
+
+
+socket.on('playerJoined', function(player) {
+
+    $('#chatList ul').append("<li> Player <strong>"+player+"</strong> joined!</li>");
+})
+
+
+$('#chatBar input').keypress(function (e) {
+    if (e.which == 13) {
+        socket.emit('chat', $('#chatBar input').val());
+    }
+});
